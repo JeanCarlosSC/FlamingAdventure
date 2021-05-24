88b2e88eb2f066d0d9dfd84d2717f3aa67af3fd1
JeanCarlosSC <jeancarlosodi@gmail.com>
2021-05-24 13:28:50 -0500

    clean code

--- app/src/main/java/com/jeank/flamingadventure/MainActivity.kt
+++ app/src/main/java/com/jeank/flamingadventure/MainActivity.kt
@@ -57,9 +57,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun hideSystemUI() {
-        // Enables regular immersive mode.
-        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
-        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
                 // Set the content to appear under the system bars so that the
                 // content doesn't resize when the system bars hide and show.
