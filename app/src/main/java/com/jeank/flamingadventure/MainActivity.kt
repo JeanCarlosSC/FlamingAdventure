74cf5b2a7a7874404064db80625cfb24742ab674
JeanCarlosSC <jeancarlosodi@gmail.com>
2021-05-24 10:26:07 -0500

    implemented editor button and added myLevelsActivity empty (#4)

--- app/src/main/java/com/jeank/flamingadventure/MainActivity.kt
+++ app/src/main/java/com/jeank/flamingadventure/MainActivity.kt
@@ -2,6 +2,7 @@ package com.jeank.flamingadventure
 
 import android.R
 import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -78,6 +79,14 @@ class MainActivity : AppCompatActivity() {
         window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+    }
+
+    /**
+     * Called when the user taps the editor button
+     */
+    fun toMyLevelsActivity(view: View) {
+        val intent = Intent(this, MyLevelsActivity::class.java)
+        startActivity(intent)
     }
 
 }
