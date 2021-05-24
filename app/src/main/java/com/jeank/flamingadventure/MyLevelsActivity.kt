3f9d1dfeed4a0b7bd41419e25f806c9af27719b8
JeanCarlosSC <jeancarlosodi@gmail.com>
2021-05-24 13:30:14 -0500

    added 'back to main' button in 'my levels activity'

--- app/src/main/java/com/jeank/flamingadventure/MyLevelsActivity.kt
+++ app/src/main/java/com/jeank/flamingadventure/MyLevelsActivity.kt
@@ -21,9 +21,6 @@ class MyLevelsActivity : AppCompatActivity() {
     }
 
     private fun hideSystemUI() {
-        // Enables regular immersive mode.
-        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
-        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
                 // Set the content to appear under the system bars so that the
                 // content doesn't resize when the system bars hide and show.
@@ -37,4 +34,7 @@ class MyLevelsActivity : AppCompatActivity() {
                 or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
     }
 
+    fun toMainActivity(view: View) {
+        onBackPressed()
+    }
 }
