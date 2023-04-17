5963cb636852e4b12c4fa01a93fcc1848257ea8d
JeanCarlosSC <jeancarlosodi@gmail.com>
2023-04-17 12:46:52 -0500

    creating prefab

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     public float horizontalInput;
-    
+    public float speed = 10.0f;
+    public float xRange = 15;
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        // keep the player in bounds
+        if(transform.position.x < -xRange) {
+            transform.position = new Vector3(-15, transform.position.y, transform.position.z);
+        }
+        else if(transform.position.x > xRange) {
+            transform.position = new Vector3(15, transform.position.y, transform.position.z);
+        }
     }
 }
